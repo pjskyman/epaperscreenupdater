e46c061465fdc394d41cbf5ff99c8e82069ebb71
Pierre-Jean Asmus <pj.asmus@terrasol.com>
2020-02-07 11:39:14 +0100

    WasherSupervisionPage

--- src/sky/epaperscreenupdater/WasherSupervisionPage.java
+++ src/sky/epaperscreenupdater/WasherSupervisionPage.java
@@ -1,9 +1,7 @@
 package sky.epaperscreenupdater;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,12 +12,9 @@ import sky.program.Duration;
 
 public class WasherSupervisionPage extends AbstractSinglePage
 {
-    private long lastRefreshTime;
-
     public WasherSupervisionPage(Page parentPage)
     {
         super(parentPage);
-        lastRefreshTime=0L;
     }
 
     public String getName()
@@ -27,90 +22,78 @@ public class WasherSupervisionPage extends AbstractSinglePage
         return "Surveillance LV/LL";
     }
 
-    public synchronized Page potentiallyUpdate()
+    protected RefreshType getRefreshType()
     {
-        long now=System.currentTimeMillis();
-        if(now-lastRefreshTime>Duration.of(1).minutePlus(7).second())
+        return RefreshType.PARTIAL_REFRESH;
+    }
+
+    protected long getMinimalRefreshDelay()
+    {
+        return Duration.of(1).minutePlus(7).second();
+    }
+
+    protected void populateImage(Graphics2D g2d) throws VetoException,Exception
+    {
+        Font baseFont=Main.FREDOKA_ONE_FONT.deriveFont(18f);
+        g2d.setFont(baseFont);
+        long baseTime=System.currentTimeMillis();
+        List<WasherInstantaneousConsumption> consumptions=loadWasherInstantaneousConsumptions(baseTime-Duration.of(1).day(),baseTime+Duration.of(10).second());
+        //WasherInstantaneousConsumption lastNullConsumption=null;
+        WasherInstantaneousConsumption lastLowConsumption=null;
+        WasherInstantaneousConsumption lastWorkingConsumption=null;
+        for(int i=consumptions.size()-1;i>=0;i--)
         {
-            Logger.LOGGER.info("Page \""+getName()+"\" needs to be updated");
-            lastRefreshTime=now;
-            try
+            WasherInstantaneousConsumption consumption=consumptions.get(i);
+            if(lastLowConsumption==null&&consumption.getConsumption()>0)
+                lastLowConsumption=consumption;
+            if(lastWorkingConsumption==null&&consumption.getConsumption()>=10)
+                lastWorkingConsumption=consumption;
+            if(lastLowConsumption!=null&&lastWorkingConsumption!=null)
+                break;
+        }
+        String message1;
+        String message2;
+        if(lastLowConsumption==null&&lastWorkingConsumption==null)
+        {
+            message1="Pas de cycle lancé récemment";
+            message2="";
+        }
+        else
+            if(lastWorkingConsumption!=null&&lastLowConsumption!=null)
             {
-                BufferedImage sourceImage=new BufferedImage(296,128,BufferedImage.TYPE_INT_ARGB_PRE);
-                Graphics2D g2d=sourceImage.createGraphics();
-                g2d.setColor(Color.WHITE);
-                g2d.fillRect(0,0,296,128);
-                g2d.setColor(Color.BLACK);
-                Font baseFont=Main.FREDOKA_ONE_FONT.deriveFont(18f);
-                g2d.setFont(baseFont);
-                long baseTime=System.currentTimeMillis();
-                List<WasherInstantaneousConsumption> consumptions=loadWasherInstantaneousConsumptions(baseTime-Duration.of(1).day(),baseTime+Duration.of(10).second());
-                //WasherInstantaneousConsumption lastNullConsumption=null;
-                WasherInstantaneousConsumption lastLowConsumption=null;
-                WasherInstantaneousConsumption lastWorkingConsumption=null;
-                for(int i=consumptions.size()-1;i>=0;i--)
+                long offset1=baseTime-lastWorkingConsumption.getTime();
+                long offset2=baseTime-lastLowConsumption.getTime();
+                if(offset1<Duration.of(1).minute())
                 {
-                    WasherInstantaneousConsumption consumption=consumptions.get(i);
-                    if(lastLowConsumption==null&&consumption.getConsumption()>0)
-                        lastLowConsumption=consumption;
-                    if(lastWorkingConsumption==null&&consumption.getConsumption()>=10)
-                        lastWorkingConsumption=consumption;
-                    if(lastLowConsumption!=null&&lastWorkingConsumption!=null)
-                        break;
-                }
-                String message1;
-                String message2;
-                if(lastLowConsumption==null&&lastWorkingConsumption==null)
-                {
-                    message1="Pas de cycle lancé récemment";
+                    message1="Cycle en cours";
                     message2="";
                 }
                 else
-                    if(lastWorkingConsumption!=null&&lastLowConsumption!=null)
-                    {
-                        long offset1=baseTime-lastWorkingConsumption.getTime();
-                        long offset2=baseTime-lastLowConsumption.getTime();
-                        if(offset1<Duration.of(1).minute())
-                        {
-                            message1="Cycle en cours";
-                            message2="";
-                        }
-                        else
-                        {
-                            if(offset1<Duration.of(1).hour())
-                                message1="Cycle terminé depuis "+offset1/60000L+" minute(s)";
-                            else
-                                message1="Cycle terminé depuis "+offset1/3600000L+" heure(s)";
-                            if(offset2<Duration.of(1).minute())
-                                message2="Machine encore allumée";
-                            else
-                                message2="";
-                        }
-                    }
+                {
+                    if(offset1<Duration.of(1).hour())
+                        message1="Cycle terminé depuis "+offset1/60000L+" minute(s)";
                     else
-                    {
-                        message1="?";
-                        message2="?";
-                    }
-//                int clockStringWidth=(int)Math.ceil(baseFont.getStringBounds(clockString,g2d.getFontRenderContext()).getWidth());
-//                int clockStringHeight=(int)Math.ceil(baseFont.getStringBounds(clockString,g2d.getFontRenderContext()).getHeight());
-                g2d.drawString(message1,2,18);
-                g2d.drawString(message2,2,36);
-                g2d.dispose();
-//                try(OutputStream outputStream=new FileOutputStream(new File("washer_supervision.png")))
-//                {
-//                    ImageIO.write(sourceImage,"png",outputStream);
-//                }
-                pixels=new Pixels(RefreshType.PARTIAL_REFRESH).writeImage(sourceImage);
-                Logger.LOGGER.info("Page \""+getName()+"\" updated successfully");
+                        message1="Cycle terminé depuis "+offset1/3600000L+" heure(s)";
+                    if(offset2<Duration.of(1).minute())
+                        message2="Machine encore allumée";
+                    else
+                        message2="";
+                }
             }
-            catch(Exception e)
+            else
             {
-                Logger.LOGGER.error("Unknown error when updating page \""+getName()+"\"");
-                e.printStackTrace();
+                message1="?";
+                message2="?";
             }
-        }
-        return this;
+//        int clockStringWidth=(int)Math.ceil(baseFont.getStringBounds(clockString,g2d.getFontRenderContext()).getWidth());
+//        int clockStringHeight=(int)Math.ceil(baseFont.getStringBounds(clockString,g2d.getFontRenderContext()).getHeight());
+        g2d.drawString(message1,2,18);
+        g2d.drawString(message2,2,36);
+    }
+
+    protected String getDebugImageFileName()
+    {
+        return "washer_supervision.png";
     }
 
     public static void main(String[] args)
