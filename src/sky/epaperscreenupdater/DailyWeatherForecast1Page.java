e022e02d76b0e961aeae608332efee47faabbd8c
Pierre-Jean Asmus <pj.asmus@terrasol.com>
2020-02-07 10:33:45 +0100

    DailyWeatherForecast1Page

--- src/sky/epaperscreenupdater/DailyWeatherForecast1Page.java
+++ src/sky/epaperscreenupdater/DailyWeatherForecast1Page.java
@@ -6,7 +6,6 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.geom.Path2D;
-import java.awt.image.BufferedImage;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,12 +17,9 @@ import sky.program.Duration;
 
 public class DailyWeatherForecast1Page extends AbstractWeatherForecastPage
 {
-    private long lastRefreshTime;
-
     public DailyWeatherForecast1Page(Page parentPage)
     {
         super(parentPage);
-        lastRefreshTime=0L;
     }
 
     public String getName()
@@ -31,169 +27,157 @@ public class DailyWeatherForecast1Page extends AbstractWeatherForecastPage
         return "Prévisions météo 1/2";
     }
 
-    public synchronized Page potentiallyUpdate()
+    protected RefreshType getRefreshType()
     {
-        long now=System.currentTimeMillis();
-        if(now-lastRefreshTime>Duration.of(4).minutePlus(9).second())
+        return RefreshType.PARTIAL_REFRESH;
+    }
+
+    protected long getMinimalRefreshDelay()
+    {
+        return Duration.of(4).minutePlus(9).second();
+    }
+
+    protected void populateImage(Graphics2D g2d) throws Exception
+    {
+        List<Daily> dailies=getLastDailies();
+        for(int x=51;x<296;x+=35)
+            g2d.drawLine(x,0,x,127);
+        for(int y=20;y<128;y+=12)
+            g2d.drawLine(0,y,295,y);
+        Font baseFont=Main.FREDOKA_ONE_FONT.deriveFont(12f);
+
+        g2d.setFont(baseFont);
+
+        String string="Date";
+        int stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,14);
+
+        string="Icône";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,31);
+
+        string="°CMin";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,43);
+
+        string="°CMax";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,55);
+
+        string="%Hum";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,67);
+
+        string="°CRosée";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,79);
+
+        string="Vent";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,91);
+
+        string="mmPluie";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,103);
+
+        string="%Pluie";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,115);
+
+        string="hPa";
+        stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+        g2d.drawString(string,26-stringWidth/2,127);
+
+        Stroke largeStroke=new BasicStroke(2.5f);
+        Stroke normalStroke=new BasicStroke();
+        g2d.setStroke(normalStroke);
+
+        for(int i=0;i<dailies.size();i++)
         {
-            Logger.LOGGER.info("Page \""+getName()+"\" needs to be updated");
-            lastRefreshTime=now;
-            List<Daily> dailies=getLastDailies();
-            try
-            {
-                BufferedImage sourceImage=new BufferedImage(296,128,BufferedImage.TYPE_INT_ARGB_PRE);
-                Graphics2D g2d=sourceImage.createGraphics();
-                g2d.setColor(Color.WHITE);
-                g2d.fillRect(0,0,296,128);
-                g2d.setColor(Color.BLACK);
-                for(int x=51;x<296;x+=35)
-                    g2d.drawLine(x,0,x,127);
-                for(int y=20;y<128;y+=12)
-                    g2d.drawLine(0,y,295,y);
-                Font baseFont=Main.FREDOKA_ONE_FONT.deriveFont(12f);
+            int baseX=51+i*35;
+            Daily daily=dailies.get(i);
 
-                g2d.setFont(baseFont);
+            GregorianCalendar calendar=new GregorianCalendar();
+            calendar.setTimeInMillis(daily.getTime());
+            string=calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT,Locale.FRANCE);
+            string=string.substring(0,1).toUpperCase()+string.substring(1);
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,9);
 
-                String string="Date";
-                int stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,14);
+            string=SimpleDateFormat.getDateInstance(DateFormat.SHORT).format(new Date(daily.getTime())).substring(0,2);
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,19);
 
-                string="Icône";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,31);
+            g2d.drawImage(Icons.getIcon(daily.getIcon()),baseX+1,21,null);
 
-                string="°CMin";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,43);
+            string=DECIMAL_0_FORMAT.format(daily.getTemperatureMin());
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,43);
 
-                string="°CMax";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,55);
+            string=DECIMAL_0_FORMAT.format(daily.getTemperatureMax());
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,55);
 
-                string="%Hum";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,67);
+            string=INTEGER_FORMAT.format(daily.getHumidity()*100d);
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
+            g2d.drawString(string,baseX+18-stringWidth/2,67);
+            g2d.fillOval(baseX+18+stringWidth/2-12/2-4,67-12/2-5,12,12);
+            g2d.setColor(Color.WHITE);
+            g2d.fillArc(baseX+18+stringWidth/2-10/2-4,67-10/2-5,10,10,90,(int)(360d-daily.getHumidity()*360d));
+            g2d.setColor(Color.BLACK);
 
-                string="°CRosée";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,79);
+            string=DECIMAL_0_FORMAT.format(daily.getDewPoint());
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,79);
 
-                string="Vent";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,91);
+            string=INTEGER_FORMAT.format(daily.getWindGust());
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
+            g2d.drawString(string,baseX+18-stringWidth/2,91);
+            g2d.setStroke(largeStroke);
+            g2d.drawLine(
+                    baseX+18+stringWidth/2-4+(int)(Math.sin((double)daily.getWindBearing()*Math.PI/180d)*6d),
+                    91-5-(int)(Math.cos((double)daily.getWindBearing()*Math.PI/180d)*6d),
+                    baseX+18+stringWidth/2-4-(int)(Math.sin((double)daily.getWindBearing()*Math.PI/180d)*4d),
+                    91-5+(int)(Math.cos((double)daily.getWindBearing()*Math.PI/180d)*4d)
+            );
+            g2d.setStroke(normalStroke);
+            Path2D path=new Path2D.Double();
+            path.moveTo(
+                    (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)daily.getWindBearing()*Math.PI/180d)*6d,
+                    91d-5d+Math.cos((double)daily.getWindBearing()*Math.PI/180d)*6d
+            );
+            path.lineTo(
+                    (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)(daily.getWindBearing()+90)*Math.PI/180d)*5d,
+                    91d-5d+Math.cos((double)(daily.getWindBearing()+90)*Math.PI/180d)*5d
+            );
+            path.lineTo(
+                    (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)(daily.getWindBearing()-90)*Math.PI/180d)*5d,
+                    91d-5d+Math.cos((double)(daily.getWindBearing()-90)*Math.PI/180d)*5d
+            );
+            path.closePath();
+            g2d.fill(path);
 
-                string="mmPluie";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,103);
+            string=DECIMAL_0_FORMAT.format(daily.getPrecipIntensity()*24d);
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,103);
 
-                string="%Pluie";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,115);
+            string=INTEGER_FORMAT.format(daily.getPrecipProbability()*100d);
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
+            g2d.drawString(string,baseX+18-stringWidth/2,115);
+            g2d.fillOval(baseX+18+stringWidth/2-12/2-4,115-12/2-5,12,12);
+            g2d.setColor(Color.WHITE);
+            g2d.fillArc(baseX+18+stringWidth/2-10/2-4,115-10/2-5,10,10,90,(int)(360d-daily.getPrecipProbability()*360d));
+            g2d.setColor(Color.BLACK);
 
-                string="hPa";
-                stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                g2d.drawString(string,26-stringWidth/2,127);
-
-                Stroke largeStroke=new BasicStroke(2.5f);
-                Stroke normalStroke=new BasicStroke();
-                g2d.setStroke(normalStroke);
-
-                for(int i=0;i<dailies.size();i++)
-                {
-                    int baseX=51+i*35;
-                    Daily daily=dailies.get(i);
-
-                    GregorianCalendar calendar=new GregorianCalendar();
-                    calendar.setTimeInMillis(daily.getTime());
-                    string=calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT,Locale.FRANCE);
-                    string=string.substring(0,1).toUpperCase()+string.substring(1);
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,9);
-
-                    string=SimpleDateFormat.getDateInstance(DateFormat.SHORT).format(new Date(daily.getTime())).substring(0,2);
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,19);
-
-                    g2d.drawImage(Icons.getIcon(daily.getIcon()),baseX+1,21,null);
-
-                    string=DECIMAL_0_FORMAT.format(daily.getTemperatureMin());
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,43);
-
-                    string=DECIMAL_0_FORMAT.format(daily.getTemperatureMax());
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,55);
-
-                    string=INTEGER_FORMAT.format(daily.getHumidity()*100d);
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
-                    g2d.drawString(string,baseX+18-stringWidth/2,67);
-                    g2d.fillOval(baseX+18+stringWidth/2-12/2-4,67-12/2-5,12,12);
-                    g2d.setColor(Color.WHITE);
-                    g2d.fillArc(baseX+18+stringWidth/2-10/2-4,67-10/2-5,10,10,90,(int)(360d-daily.getHumidity()*360d));
-                    g2d.setColor(Color.BLACK);
-
-                    string=DECIMAL_0_FORMAT.format(daily.getDewPoint());
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,79);
-
-                    string=INTEGER_FORMAT.format(daily.getWindGust());
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
-                    g2d.drawString(string,baseX+18-stringWidth/2,91);
-                    g2d.setStroke(largeStroke);
-                    g2d.drawLine(
-                            baseX+18+stringWidth/2-4+(int)(Math.sin((double)daily.getWindBearing()*Math.PI/180d)*6d),
-                            91-5-(int)(Math.cos((double)daily.getWindBearing()*Math.PI/180d)*6d),
-                            baseX+18+stringWidth/2-4-(int)(Math.sin((double)daily.getWindBearing()*Math.PI/180d)*4d),
-                            91-5+(int)(Math.cos((double)daily.getWindBearing()*Math.PI/180d)*4d)
-                    );
-                    g2d.setStroke(normalStroke);
-                    Path2D path=new Path2D.Double();
-                    path.moveTo(
-                            (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)daily.getWindBearing()*Math.PI/180d)*6d,
-                            91d-5d+Math.cos((double)daily.getWindBearing()*Math.PI/180d)*6d
-                    );
-                    path.lineTo(
-                            (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)(daily.getWindBearing()+90)*Math.PI/180d)*5d,
-                            91d-5d+Math.cos((double)(daily.getWindBearing()+90)*Math.PI/180d)*5d
-                    );
-                    path.lineTo(
-                            (double)baseX+18d+(double)stringWidth/2d-4d-Math.sin((double)(daily.getWindBearing()-90)*Math.PI/180d)*5d,
-                            91d-5d+Math.cos((double)(daily.getWindBearing()-90)*Math.PI/180d)*5d
-                    );
-                    path.closePath();
-                    g2d.fill(path);
-
-                    string=DECIMAL_0_FORMAT.format(daily.getPrecipIntensity()*24d);
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,103);
-
-                    string=INTEGER_FORMAT.format(daily.getPrecipProbability()*100d);
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth())+12;
-                    g2d.drawString(string,baseX+18-stringWidth/2,115);
-                    g2d.fillOval(baseX+18+stringWidth/2-12/2-4,115-12/2-5,12,12);
-                    g2d.setColor(Color.WHITE);
-                    g2d.fillArc(baseX+18+stringWidth/2-10/2-4,115-10/2-5,10,10,90,(int)(360d-daily.getPrecipProbability()*360d));
-                    g2d.setColor(Color.BLACK);
-
-                    string=INTEGER_FORMAT.format(daily.getPressure());
-                    stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
-                    g2d.drawString(string,baseX+18-stringWidth/2,127);
-                }
-                g2d.dispose();
-//                try(OutputStream outputStream=new FileOutputStream(new File("weather1.png")))
-//                {
-//                    ImageIO.write(sourceImage,"png",outputStream);
-//                }
-                pixels=new Pixels(RefreshType.PARTIAL_REFRESH).writeImage(sourceImage);
-                Logger.LOGGER.info("Page \""+getName()+"\" updated successfully");
-            }
-            catch(Exception e)
-            {
-                Logger.LOGGER.error("Unknown error when updating page \""+getName()+"\"");
-                e.printStackTrace();
-            }
+            string=INTEGER_FORMAT.format(daily.getPressure());
+            stringWidth=(int)Math.ceil(baseFont.getStringBounds(string,g2d.getFontRenderContext()).getWidth());
+            g2d.drawString(string,baseX+18-stringWidth/2,127);
         }
-        return this;
+    }
+
+    protected String getDebugImageFileName()
+    {
+        return "weather1.png";
     }
 
     public static void main(String[] args)
