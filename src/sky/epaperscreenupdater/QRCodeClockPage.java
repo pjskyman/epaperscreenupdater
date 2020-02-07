363bf1220338393665c7c65d5bd42dac178cdba9
Pierre-Jean Asmus <pj.asmus@terrasol.com>
2020-02-07 11:33:15 +0100

    QRCodeClockPage

--- src/sky/epaperscreenupdater/QRCodeClockPage.java
+++ src/sky/epaperscreenupdater/QRCodeClockPage.java
@@ -1,6 +1,5 @@
 package sky.epaperscreenupdater;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
@@ -13,12 +12,9 @@ import sky.program.Duration;
 
 public class QRCodeClockPage extends AbstractSinglePage
 {
-    private long lastRefreshTime;
-
     public QRCodeClockPage(Page parentPage)
     {
         super(parentPage);
-        lastRefreshTime=0L;
     }
 
     public String getName()
@@ -26,40 +22,28 @@ public class QRCodeClockPage extends AbstractSinglePage
         return "Horloge QR code";
     }
 
-    public synchronized Page potentiallyUpdate()
+    protected RefreshType getRefreshType()
     {
-        long now=System.currentTimeMillis();
-        if(now-lastRefreshTime>Duration.of(1).second())
-        {
-            Logger.LOGGER.info("Page \""+getName()+"\" needs to be updated");
-            lastRefreshTime=now;
-            try
-            {
-                BufferedImage sourceImage=new BufferedImage(296,128,BufferedImage.TYPE_INT_ARGB_PRE);
-                Graphics2D g2d=sourceImage.createGraphics();
-                g2d.setColor(Color.WHITE);
-                g2d.fillRect(0,0,296,128);
-                g2d.setColor(Color.BLACK);
-                byte[] binary=QRCode.from(SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM).format(new Date())).stream().toByteArray();
-                BufferedImage image=ImageIO.read(new ByteArrayInputStream(binary));
-                AffineTransform affineTransform=AffineTransform.getScaleInstance(1.5d,1.5d);
-                affineTransform.concatenate(AffineTransform.getTranslateInstance(37d,-19.5d));
-                g2d.drawImage(image,affineTransform,null);
-                g2d.dispose();
-//                try(OutputStream outputStream=new FileOutputStream(new File("qrcode.png")))
-//                {
-//                    ImageIO.write(sourceImage,"png",outputStream);
-//                }
-                pixels=new Pixels(RefreshType.PARTIAL_REFRESH_IN_FAST_MODE).writeImage(sourceImage);
-                Logger.LOGGER.info("Page \""+getName()+"\" updated successfully");
-            }
-            catch(Exception e)
-            {
-                Logger.LOGGER.error("Unknown error when updating page \""+getName()+"\"");
-                e.printStackTrace();
-            }
-        }
-        return this;
+        return RefreshType.PARTIAL_REFRESH_IN_FAST_MODE;
+    }
+
+    protected long getMinimalRefreshDelay()
+    {
+        return Duration.of(1).secondMinus(50).millisecond();
+    }
+
+    protected void populateImage(Graphics2D g2d) throws VetoException,Exception
+    {
+        byte[] binary=QRCode.from(SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM).format(new Date())).stream().toByteArray();
+        BufferedImage image=ImageIO.read(new ByteArrayInputStream(binary));
+        AffineTransform affineTransform=AffineTransform.getScaleInstance(1.5d,1.5d);
+        affineTransform.concatenate(AffineTransform.getTranslateInstance(37d,-19.5d));
+        g2d.drawImage(image,affineTransform,null);
+    }
+
+    protected String getDebugImageFileName()
+    {
+        return "qrcode.png";
     }
 
     public static void main(String[] args)
