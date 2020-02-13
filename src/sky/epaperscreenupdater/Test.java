cd75c9d9e8d82c89e19634972167750c39b918e5
pjskyman <pjskyman@free.fr>
2020-02-13 23:53:01 +0100

    Test

--- src/sky/epaperscreenupdater/Test.java
+++ src/sky/epaperscreenupdater/Test.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 import sky.epaperscreenupdater.page.AbstractPage;
 
 public class Test
@@ -12,11 +13,12 @@ public class Test
     {
         try
         {
-            EpaperScreenManager.display(getScreen(0),RefreshType.TOTAL_REFRESH);
-            Thread.sleep(2000L);
-            EpaperScreenManager.display(getScreen(1),RefreshType.PARTIAL_REFRESH);
-            Thread.sleep(2000L);
-            EpaperScreenManager.display(getScreen(2),RefreshType.PARTIAL_REFRESH);
+            for(int i=0;i<=10;i++)
+            {
+                EpaperScreenManager.display(getScreen(i),i==0?RefreshType.TOTAL_REFRESH:RefreshType.PARTIAL_REFRESH);
+                if(i<10)
+                    Thread.sleep(2000L);
+            }
         }
         catch(InterruptedException e)
         {
@@ -30,6 +32,7 @@ public class Test
 
     private static Screen getScreen(int count)
     {
+        Random random=new Random();
         Screen screen=new Screen();
         Graphics2D g2d=null;
         try
@@ -43,10 +46,12 @@ public class Test
             g2d.drawLine(0,0,0,1);
             Font baseFont=AbstractPage.FREDOKA_ONE_FONT.deriveFont(13f);
             g2d.setFont(baseFont);
-            g2d.drawString("EpaperScreenUpdater by PJ Skyman",55,24);
-            g2d.drawString("A Pie Jam Sun production",55,44);
-            g2d.drawString("©2018-2020",55,64);
-            g2d.drawString(""+count,55,84);
+            g2d.drawString("EpaperScreenUpdater by PJ Skyman",random.nextInt(150),20+random.nextInt(10));
+            g2d.drawString("A Pie Jam Sun production",random.nextInt(150),40+random.nextInt(10));
+            g2d.drawString("©2018-2020",random.nextInt(150),60+random.nextInt(10));
+            g2d.drawString(""+count,random.nextInt(150),80+random.nextInt(10));
+            g2d.drawLine(random.nextInt(296),random.nextInt(128),random.nextInt(296),random.nextInt(128));
+            g2d.drawLine(random.nextInt(296),random.nextInt(128),random.nextInt(296),random.nextInt(128));
             g2d.dispose();
             screen.setImage(image);
         }
