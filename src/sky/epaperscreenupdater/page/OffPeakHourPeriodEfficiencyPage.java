0e2f615a5e53dcba7c04bb1509dc22cf038f3c43
Pierre-Jean Asmus <pj.asmus@terrasol.com>
2020-02-20 10:18:43 +0100

    Adaptations suite à l'arrivée de Linky.

--- src/sky/epaperscreenupdater/page/OffPeakHourPeriodEfficiencyPage.java
+++ src/sky/epaperscreenupdater/page/OffPeakHourPeriodEfficiencyPage.java
@@ -31,8 +31,7 @@ public class OffPeakHourPeriodEfficiencyPage extends AbstractSinglePage
 
         GregorianCalendar calendar=new GregorianCalendar();
         int nowHour=calendar.get(Calendar.HOUR_OF_DAY);
-        int nowMinute=calendar.get(Calendar.MINUTE);
-        if(nowHour<6||nowHour==6&&nowMinute<2)
+        if(nowHour<6)
             calendar.setTimeInMillis(calendar.getTimeInMillis()-Duration.of(1).day());
         int todayYear=calendar.get(Calendar.YEAR);
         int todayMonth=calendar.get(Calendar.MONTH)+1;
