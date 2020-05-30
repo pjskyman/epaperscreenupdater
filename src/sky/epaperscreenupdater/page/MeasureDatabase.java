34226ff650621dc521c9d7ecbd5681a4de70d5a7
pjskyman <pjskyman@free.fr>
2020-05-30 11:36:12 +0200

    Synchronisation des opérations dans la base de données des mesures Netatmo.

--- src/sky/epaperscreenupdater/page/MeasureDatabase.java
+++ src/sky/epaperscreenupdater/page/MeasureDatabase.java
@@ -11,6 +11,7 @@ import sky.program.Duration;
 public class MeasureDatabase
 {
     private final Map<MeasureDatabaseKey,Measure> measures;
+    private static final Object LOCK_OBJECT=new Object();
 
     public MeasureDatabase()
     {
@@ -19,28 +20,37 @@ public class MeasureDatabase
 
     public void addMeasures(String measureKind,Measure[] measures)
     {
-        Arrays.stream(measures)
-                .forEach(measure->this.measures.put(new MeasureDatabaseKey(measure.getDate().getTime(),measureKind),measure));
+        synchronized(LOCK_OBJECT)
+        {
+            Arrays.stream(measures)
+                    .forEach(measure->this.measures.put(new MeasureDatabaseKey(measure.getDate().getTime(),measureKind),measure));
+        }
     }
 
     public void clean()
     {
-        int sizeBefore=measures.size();
-        long now=System.currentTimeMillis();
-        measures.keySet().stream()
-                .filter(key->now-key.getTime()>Duration.of(1).dayPlus(3).hour())
-                .forEach(measures::remove);
-        Logger.LOGGER.info("Database cleaned in "+(System.currentTimeMillis()-now)+" ms, "+(sizeBefore-measures.size())+" measures deleted");
+        synchronized(LOCK_OBJECT)
+        {
+            int sizeBefore=measures.size();
+            long now=System.currentTimeMillis();
+            measures.keySet().stream()
+                    .filter(key->now-key.getTime()>Duration.of(1).dayPlus(3).hour())
+                    .forEach(measures::remove);
+            Logger.LOGGER.info("Database cleaned in "+(System.currentTimeMillis()-now)+" ms, "+(sizeBefore-measures.size())+" measures deleted");
+        }
     }
 
     public Measure[] getMeasures(String measureKind,long from,long to)
     {
-        return measures.entrySet().stream()
-                .filter(entry->entry.getKey().getMeasureKind().equals(measureKind))
-                .filter(entry->entry.getKey().getTime()>=from)
-                .filter(entry->entry.getKey().getTime()<=to)
-                .map(Entry::getValue)
-                .sorted()
-                .toArray(Measure[]::new);
+        synchronized(LOCK_OBJECT)
+        {
+            return measures.entrySet().stream()
+                    .filter(entry->entry.getKey().getMeasureKind().equals(measureKind))
+                    .filter(entry->entry.getKey().getTime()>=from)
+                    .filter(entry->entry.getKey().getTime()<=to)
+                    .map(Entry::getValue)
+                    .sorted()
+                    .toArray(Measure[]::new);
+        }
     }
 }
